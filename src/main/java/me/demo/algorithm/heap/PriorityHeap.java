44744f5e521896e6f66a09ce39228b0bb300e8ff
王刚 <geosmart@foxmail.com>
2019-08-22 21:19:14 +0800

    update heap

--- src/main/java/me/demo/algorithm/heap/PriorityHeap.java
+++ src/main/java/me/demo/algorithm/heap/PriorityHeap.java
@@ -2,8 +2,11 @@ package me.demo.algorithm.heap;
 
 import com.alibaba.fastjson.JSON;
 
+import java.util.Arrays;
+
 /**
  * 二叉堆
+ * 示例日志：https://gist.github.com/geosmart/c31fe452f0b536ea12fbda72c1385553
  * //# 参考：
  * // [漫画：什么是堆排序？](https://www.itcodemonkey.com/article/8929.html)
  * // [漫画：什么是二叉堆？](https://www.itcodemonkey.com/article/8660.html)
@@ -22,39 +25,53 @@ public class PriorityHeap {
     public static void main(String[] args) {
         int[] array = new int[]{9, 8, 6, 1, 4, 5, 3, 2, 7};
         PriorityHeap heap = new PriorityHeap(array);
-        System.out.println(String.format("origin heap[%s]", JSON.toJSONString(heap.getHeapArray())));
 
-        int parentIdx = array.length / 2 - 1;
-        heap.heapify(parentIdx);
-        System.out.println(String.format("heapify-%s", JSON.toJSONString(heap.getHeapArray())));
+        System.out.println("orgin heap.");
+        heap.dump();
 
-        int node = 0;
+        heap.heapify();
+
+        heap.heapSort();
+
+        int node = 111;
         heap.insert(node);
-        System.out.println(String.format("insert node[%s],heap-%s", node, JSON.toJSONString(heap.getHeapArray())));
+
+        heap.heapSort();
 
         int heapSize = heap.getHeapArray().length;
         for (int i = 0; i < heapSize; i++) {
             int root = heap.pop();
-            System.out.println(String.format("pop node[%s],heap-%s", root, JSON.toJSONString(heap.getHeapArray().length)));
+            System.out.println(String.format("pop node[%s],heap-%s", root, JSON.toJSONString(heap.getHeapArray())));
         }
+    }
+
+    public void heapify() {
+        int parentIdx = getLastParentNode();
+        heapify(parentIdx);
+        System.out.println("heapify result.");
+        dump();
     }
 
     /***
      * 构建堆
-     * @param parentIdx
+     * @param parentIdx 父节点索引
      */
     private void heapify(int parentIdx) {
         //从最后一个父节点，逐级向上，遍历到根节点
         while (parentIdx >= 0) {
             //求父节点与左右孩子的最小值
             int minIdx = getMinNode(parentIdx);
+            System.out.println(String.format("calc sub tree of parent(%s),min=%s,need swap=%s", heapArray[parentIdx], heapArray[minIdx], (minIdx != parentIdx)));
             //交换位置：最小值不在父节点时
             if (minIdx != parentIdx) {
                 swap(heapArray, parentIdx, minIdx);
-                System.out.println(String.format("swap[%s-%s],%s,parent[%s]", parentIdx, minIdx, JSON.toJSONString(heapArray), parentIdx));
+                System.out.println(String.format("swap index %s<->%s", heapArray[parentIdx], heapArray[minIdx]));
+                dump();
+                System.out.println(String.format("sub tree of parent[%s] is leaf node? %s", heapArray[minIdx], getLeftChild(minIdx) == -1));
                 //左孙子为叶子结点：以当前最小结点作为父节点，调整树使其满足parent比children小
-                if (getLeftIdx(heapArray, minIdx) < heapArray.length - 1) {
+                if (getLeftChild(minIdx) != -1) {
                     heapify(minIdx);
+                    dump();
                 }
             }
             parentIdx = parentIdx - 1;
@@ -63,7 +80,7 @@ public class PriorityHeap {
 
     /***
      * 插入节点
-     * @param node
+     * @param node 节点值
      */
     public void insert(int node) {
         System.out.println(String.format("insert node[%s]", node));
@@ -76,6 +93,7 @@ public class PriorityHeap {
         int parentIdx = newArray.length / 2 - 1;
         heapArray = newArray;
         heapify(parentIdx);
+        System.out.println(String.format("insert node[%s],heap-%s", node, JSON.toJSONString(heapArray)));
     }
 
     /***
@@ -107,14 +125,19 @@ public class PriorityHeap {
 
     /***
      * 堆排序
-     * @param array
      */
-    public void heapSort(int[] array) {
-        //todo 以重复pop实现堆排序
-    }
+    public void heapSort() {
+        //构建堆
+        int parentIdx = getLastParentNode();
+        heapify(parentIdx);
 
-    public int size() {
-        return heapArray.length;
+        int[] sortArray = new int[heapArray.length];
+        //逐个弹出堆顶最值即可完成排序
+        for (int i = 0; i < sortArray.length; i++) {
+            sortArray[i] = pop();
+        }
+        heapArray = sortArray;
+        System.out.println(String.format("heapSort-%s", JSON.toJSONString(heapArray)));
     }
 
     /***
@@ -123,18 +146,57 @@ public class PriorityHeap {
      * @return
      */
     private int getMinNode(int parentIdx) {
-        int leftIdx = getLeftIdx(heapArray, parentIdx);
-        int rightIdx = getRightIdx(heapArray, parentIdx);
+        int leftIdx = getLeftChild(parentIdx);
+        int rightIdx = getRightChild(parentIdx);
         int minIdx;
-        if (heapArray[leftIdx] < heapArray[rightIdx]) {
-            minIdx = leftIdx;
-        } else {
-            minIdx = rightIdx;
-        }
-        if (heapArray[parentIdx] < heapArray[minIdx]) {
-            minIdx = parentIdx;
-        }
+        minIdx = getMinIdx(leftIdx, rightIdx);
+        minIdx = getMinIdx(minIdx, parentIdx);
         return minIdx;
+    }
+
+    private int getMinIdx(int idx1, int idx2) {
+        if (idx1 == -1) {
+            return idx2;
+        } else if (idx2 == -1) {
+            return idx1;
+        }
+
+        if (heapArray[idx1] < heapArray[idx2]) {
+            return idx1;
+        } else {
+            return idx2;
+        }
+    }
+
+    /***
+     * 获取二叉堆的最后一个父节点的index
+     * @return
+     */
+    private int getLastParentNode() {
+        return (heapArray.length >> 1) - 1;
+    }
+
+
+    /***
+     * 获取左孩子index
+     * @param parentIdx
+     * @return
+     */
+    private int getLeftChild(int parentIdx) {
+        int leftIdx = (parentIdx << 1) + 1;
+        leftIdx = leftIdx > heapArray.length - 1 ? -1 : leftIdx;
+        return leftIdx;
+    }
+
+    /***
+     * 获取右孩子index
+     * @param parentIdx
+     * @return
+     */
+    private int getRightChild(int parentIdx) {
+        int rightIdx = (parentIdx << 1) + 2;
+        rightIdx = rightIdx > heapArray.length - 1 ? -1 : rightIdx;
+        return rightIdx;
     }
 
     /***
@@ -146,29 +208,49 @@ public class PriorityHeap {
         a[idx1] = min;
     }
 
-    /***
-     * 获取左孩子index
-     * @param a
-     * @param parentIdx
-     * @return
-     */
-    private int getLeftIdx(int[] a, int parentIdx) {
-        int leftIdx = (2 * parentIdx + 1) > a.length - 1 ? a.length - 1 : (2 * parentIdx + 1);
-        return leftIdx;
-    }
-
-    /***
-     * 获取右孩子index
-     * @param a
-     * @param parentIdx
-     * @return
-     */
-    private int getRightIdx(int[] a, int parentIdx) {
-        int rightIdx = (2 * parentIdx + 2) > a.length - 1 ? a.length - 1 : (2 * parentIdx + 2);
-        return rightIdx;
+    public int size() {
+        return heapArray.length;
     }
 
     public int[] getHeapArray() {
         return heapArray;
     }
+
+    public void dump() {
+        System.out.println(stringOfSize(64, '-'));
+        int[] heap = new int[heapArray.length + 1];
+        System.arraycopy(heapArray, 0, heap, 1, heapArray.length);
+        int height = log2(heap.length) + 1;
+        for (int i = 1, len = heap.length; i < len; i++) {
+            int x = heap[i];
+            int level = log2(i) + 1;
+            int spaces = (height - level) * 4;
+            //FIXME tab calc error
+            if (i == len - 1) {
+                spaces = (height - level + 1) * 4;
+            }
+
+            System.out.print(stringOfSize(spaces, ' '));
+            System.out.print(x);
+
+            if ((int) Math.pow(2, level) - 1 == i) {
+                System.out.println();
+            }
+        }
+        System.out.println();
+        System.out.println(stringOfSize(64, '-'));
+    }
+
+    private String stringOfSize(int size, char ch) {
+        char[] a = new char[size];
+        Arrays.fill(a, ch);
+        return new String(a);
+    }
+
+    // log with base 2
+    private int log2(int x) {
+        // = log(x) with base 10 / log(2) with base 10
+        return (int) (Math.log(x) / Math.log(2));
+    }
+
 }
