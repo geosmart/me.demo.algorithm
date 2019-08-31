cfc968e85e32a0549f2362009ca588f294e98c1b
geosmart <geosmart@hotmail.com>
2019-08-31 12:13:17 +0800

    Update PriorityHeap.java

--- src/main/java/me/demo/algorithm/heap/PriorityHeap.java
+++ src/main/java/me/demo/algorithm/heap/PriorityHeap.java
@@ -6,263 +6,157 @@ import java.util.Arrays;
 
 /**
  * 二叉堆
- * 示例日志：https://gist.github.com/geosmart/c31fe452f0b536ea12fbda72c1385553
- * //# 参考：
- * // [漫画：什么是堆排序？](https://www.itcodemonkey.com/article/8929.html)
- * // [漫画：什么是二叉堆？](https://www.itcodemonkey.com/article/8660.html)
- * // [漫画：什么是优先队列？](https://www.itcodemonkey.com/article/9681.html)
  */
 public class PriorityHeap {
     /***
      * 堆数组
      */
-    private int[] heapArray;
+    private Object[] heap;
 
-    public PriorityHeap(int[] array) {
-        heapArray = array;
-    }
+    /***
+     * 堆大小
+     */
+    private int size;
 
-    public static void main(String[] args) {
-        int[] array = new int[]{9, 8, 6, 1, 4, 5, 3, 2, 7};
-        PriorityHeap heap = new PriorityHeap(array);
-
-        System.out.println("orgin heap.");
-        heap.dump();
-
-        heap.heapify();
-
-        heap.heapSort();
-
-        int node = 111;
-        heap.insert(node);
-
-        heap.heapSort();
-
-        int heapSize = heap.getHeapArray().length;
-        for (int i = 0; i < heapSize; i++) {
-            int root = heap.pop();
-            System.out.println(String.format("pop node[%s],heap-%s", root, JSON.toJSONString(heap.getHeapArray())));
-        }
-    }
-
-    public void heapify() {
-        int parentIdx = getLastParentNode();
-        heapify(parentIdx);
-        System.out.println("heapify result.");
-        dump();
+    public PriorityHeap(Object[] array) {
+        heap = array;
+        size = heap.length;
     }
 
     /***
-     * 构建堆
-     * @param parentIdx 父节点索引
+     * 将数组调整为满足parent>(left,right)的堆结构
      */
-    private void heapify(int parentIdx) {
-        //从最后一个父节点，逐级向上，遍历到根节点
-        while (parentIdx >= 0) {
-            //求父节点与左右孩子的最小值
-            int minIdx = getMinNode(parentIdx);
-            System.out.println(String.format("calc sub tree of parent(%s),min=%s,need swap=%s", heapArray[parentIdx], heapArray[minIdx], (minIdx != parentIdx)));
-            //交换位置：最小值不在父节点时
-            if (minIdx != parentIdx) {
-                swap(heapArray, parentIdx, minIdx);
-                System.out.println(String.format("swap index %s<->%s", heapArray[parentIdx], heapArray[minIdx]));
-                dump();
-                System.out.println(String.format("sub tree of parent[%s] is leaf node? %s", heapArray[minIdx], getLeftChild(minIdx) == -1));
-                //左孙子为叶子结点：以当前最小结点作为父节点，调整树使其满足parent比children小
-                if (getLeftChild(minIdx) != -1) {
-                    heapify(minIdx);
-                    dump();
-                }
-            }
-            parentIdx = parentIdx - 1;
+    public void heapify() {
+        System.out.println("orgin heap.");
+        printHeap();
+        //从最后一个父节点开始下沉
+        int i = (size >> 1) - 1;
+        while (i >= 0) {
+            System.out.println(String.format("heapify,parent[%s] siftdown", i));
+            siftDown(i, (int) heap[i]);
+            i--;
+            printHeap();
         }
     }
+
 
     /***
      * 插入节点
      * @param node 节点值
      */
-    public void insert(int node) {
-        System.out.println(String.format("insert node[%s]", node));
-        //grow
-        int[] newArray = new int[heapArray.length + 1];
-        System.arraycopy(heapArray, 0, newArray, 0, heapArray.length);
-        newArray[newArray.length - 1] = node;
-
-        //siftup from last parentNode
-        int parentIdx = newArray.length / 2 - 1;
-        heapArray = newArray;
-        heapify(parentIdx);
-        System.out.println(String.format("insert node[%s],heap-%s", node, JSON.toJSONString(heapArray)));
-    }
-
-
-    /***
-     * 获取堆顶节点
-     * @return
-     */
-    public int peak() {
-        if (size() == 0) {
-            throw new IndexOutOfBoundsException("heap is empty");
-        }
-        return heapArray[0];
+    public void add(int node) {
+        heap = Arrays.copyOf(heap, size + 1);
+        size = size + 1;
+        heap[size - 1] = node;
+        //从最后一个节点开始,逐父节点单线遍历上浮
+        siftUp(size - 1, (int) heap[size - 1]);
     }
 
     /***
      * 弹出节点
-     * @return
+     * @return 堆顶节点值
      */
-    public int pop() {
-        System.out.println("pop node");
-        if (size() == 0) {
+    public Object pop() {
+        if (size == 0) {
             throw new IndexOutOfBoundsException("heap is empty");
         }
-        //移除堆顶节点
-        int root = heapArray[0];
-        if (size() == 1) {
-            heapArray = new int[0];
-            return root;
+        //弹出节点
+        Object node = heap[0];
+        //堆尾节点移动到堆顶节点
+        heap[0] = heap[heap.length - 1];
+        //数组
+        heap = Arrays.copyOf(heap, size - 1);
+        size -= 1;
+        if (size > 1) {
+            //顶点下沉
+            System.out.println(String.format("parent[%s] siftdown", 0));
+            siftDown(0, (int) heap[0]);
         }
-        //堆尾节点替换为根节点
-        heapArray[0] = heapArray[heapArray.length - 1];
+        return node;
+    }
 
-        int[] newArray = new int[heapArray.length - 1];
-        System.arraycopy(heapArray, 0, newArray, 0, heapArray.length - 1);
-        heapArray = newArray;
+    /***
+     * 获取堆顶节点
+     * @return 堆顶节点值
+     */
+    public Object peak() {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("heap is empty");
+        }
+        return heap[0];
+    }
 
-        //从根节点执行下沉
-        heapify(0);
-        return root;
+    /***
+     * 节点下沉
+     * @param parent 父节点index
+     * @param value 父节点值
+     */
+    private void siftDown(int parent, int value) {
+        //当parent还有子节点时进行循环swap
+        while (parent < (size / 2)) {
+            //假设left最小
+            int left = (parent << 1) + 1;
+            int right = left + 1;
+            if (right < size && (int) heap[right] < (int) heap[left]) {
+                //right最小
+                left = right;
+            }
+            //parent最小
+            if (value < (int) heap[left]) {
+                break;
+            }
+            //交换parent与最小值
+            heap[parent] = heap[left];
+            parent = left;
+        }
+        //将原始parent放到最终交换后的位置
+        heap[parent] = value;
+    }
+
+    /***
+     * 节点上浮
+     * @param child 节点index
+     * @param value 节点值
+     */
+    private void siftUp(int child, int value) {
+        //从最后一个节点开始,逐父节点单线遍历上浮
+        while (child > 0) {
+            System.out.println(String.format("node[%s] siftup", child));
+            int parent = (child - 1) / 2;
+            //父节点大，则交换上浮
+            if ((int) heap[child] < (int) heap[parent]) {
+                heap[child] = heap[parent];
+                heap[parent] = value;
+                child = parent;
+                printHeap();
+            } else {
+                break;
+            }
+        }
     }
 
     /***
      * 堆排序
      */
-    public void heapSort() {
+    public int[] heapSort() {
         //构建堆
-        int parentIdx = getLastParentNode();
-        heapify(parentIdx);
+        heapify();
 
-        int[] sortArray = new int[heapArray.length];
+        int[] sortArray = new int[heap.length];
         //逐个弹出堆顶最值即可完成排序
         for (int i = 0; i < sortArray.length; i++) {
-            sortArray[i] = pop();
+            System.out.println(String.format("pop[%s]", i));
+            sortArray[i] = (int) pop();
         }
-        heapArray = sortArray;
-        System.out.println(String.format("heapSort-%s", JSON.toJSONString(heapArray)));
+        return sortArray;
     }
 
-    /***
-     * 获取二叉堆中某个分支（p,l,r）的最小节点index
-     * @param parentIdx
-     * @return
-     */
-    private int getMinNode(int parentIdx) {
-        int leftIdx = getLeftChild(parentIdx);
-        int rightIdx = getRightChild(parentIdx);
-        int minIdx;
-        minIdx = getMinIdx(leftIdx, rightIdx);
-        minIdx = getMinIdx(minIdx, parentIdx);
-        return minIdx;
+    public void printHeap() {
+        System.out.println(JSON.toJSONString(getHeap()));
     }
 
-    private int getMinIdx(int idx1, int idx2) {
-        if (idx1 == -1) {
-            return idx2;
-        } else if (idx2 == -1) {
-            return idx1;
-        }
-
-        if (heapArray[idx1] < heapArray[idx2]) {
-            return idx1;
-        } else {
-            return idx2;
-        }
-    }
-
-    /***
-     * 获取二叉堆的最后一个父节点的index
-     * @return
-     */
-    private int getLastParentNode() {
-        return (heapArray.length >> 1) - 1;
-    }
-
-
-    /***
-     * 获取左孩子index
-     * @param parentIdx
-     * @return
-     */
-    private int getLeftChild(int parentIdx) {
-        int leftIdx = (parentIdx << 1) + 1;
-        leftIdx = leftIdx > heapArray.length - 1 ? -1 : leftIdx;
-        return leftIdx;
-    }
-
-    /***
-     * 获取右孩子index
-     * @param parentIdx
-     * @return
-     */
-    private int getRightChild(int parentIdx) {
-        int rightIdx = (parentIdx << 1) + 2;
-        rightIdx = rightIdx > heapArray.length - 1 ? -1 : rightIdx;
-        return rightIdx;
-    }
-
-    /***
-     * 交换a中idx1和idx2的值
-     */
-    private void swap(int[] a, int idx1, int idx2) {
-        int min = a[idx2];
-        a[idx2] = a[idx1];
-        a[idx1] = min;
-    }
-
-    public int size() {
-        return heapArray.length;
-    }
-
-    public int[] getHeapArray() {
-        return heapArray;
-    }
-
-    public void dump() {
-        System.out.println(stringOfSize(64, '-'));
-        int[] heap = new int[heapArray.length + 1];
-        System.arraycopy(heapArray, 0, heap, 1, heapArray.length);
-        int height = log2(heap.length) + 1;
-        for (int i = 1, len = heap.length; i < len; i++) {
-            int x = heap[i];
-            int level = log2(i) + 1;
-            int spaces = (height - level) * 4;
-            //FIXME tab calc error
-            if (i == len - 1) {
-                spaces = (height - level + 1) * 4;
-            }
-
-            System.out.print(stringOfSize(spaces, ' '));
-            System.out.print(x);
-
-            if ((int) Math.pow(2, level) - 1 == i) {
-                System.out.println();
-            }
-        }
-        System.out.println();
-        System.out.println(stringOfSize(64, '-'));
-    }
-
-    private String stringOfSize(int size, char ch) {
-        char[] a = new char[size];
-        Arrays.fill(a, ch);
-        return new String(a);
-    }
-
-    // log with base 2
-    private int log2(int x) {
-        // = log(x) with base 10 / log(2) with base 10
-        return (int) (Math.log(x) / Math.log(2));
+    public Object[] getHeap() {
+        return heap;
     }
 
 }
